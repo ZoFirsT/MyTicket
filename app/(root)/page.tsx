aaf679c665b640cec0016476ce1bac79a3480c7a
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-01-18 05:28:45 +0700

    up

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,8 +24,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <h1 className="h1-bold">Rmutp-Ticket</h1>
+            <p className="p-regular-16 md:p-regular-20">RmutpTicket is an innovative event management platform designed specifically for RMUTP community. Offering features like ticket booking, event scheduling, and interactive maps, it serves as a one-stop solution for students and faculty to organize and participate in campus events efficiently.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
@@ -33,13 +33,13 @@ export default async function Home({ searchParams }: SearchParamProps) {
             </Button>
           </div>
 
-          <Image 
+          {/* <Image 
             src="/assets/images/hero.png"
             alt="hero"
             width={1000}
             height={1000}
             className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
-          />
+          /> */}
         </div>
       </section> 
 
