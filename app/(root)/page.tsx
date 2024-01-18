89d717a818682abdf966bf55bf4f358c0362bb63
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-01-18 07:27:10 +0700

    imagerenew

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,23 @@
-import CategoryFilter from '@/components/shared/CategoryFilter';
-import Collection from '@/components/shared/Collection'
-import Search from '@/components/shared/Search';
-import { Button } from '@/components/ui/button'
-import { getAllEvents } from '@/lib/actions/event.actions';
-import { SearchParamProps } from '@/types';
-import Image from 'next/image'
-import Link from 'next/link'
+import CategoryFilter from "@/components/shared/CategoryFilter";
+import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
+import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
-  const searchText = (searchParams?.query as string) || '';
-  const category = (searchParams?.category as string) || '';
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
 
   const events = await getAllEvents({
     query: searchText,
     category,
     page,
-    limit: 6
-  })
+    limit: 6,
+  });
 
   return (
     <>
@@ -25,33 +25,42 @@ export default async function Home({ searchParams }: SearchParamProps) {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">Rmutp-Ticket</h1>
-            <p className="p-regular-16 md:p-regular-20">RmutpTicket is an innovative event management platform designed specifically for RMUTP community. Offering features like ticket booking, event scheduling, and interactive maps, it serves as a one-stop solution for students and faculty to organize and participate in campus events efficiently.</p>
+            <p className="p-regular-16 md:p-regular-20">
+              RmutpTicket is an innovative event management platform designed
+              specifically for RMUTP community. Offering features like ticket
+              booking, event scheduling, and interactive maps, it serves as a
+              one-stop solution for students and faculty to organize and
+              participate in campus events efficiently.
+            </p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
-              <Link href="#events">
-                Explore Now
-              </Link>
+              <Link href="#events">Explore Now</Link>
             </Button>
           </div>
 
-          <Image 
+          <Image
             src="/assets/images/hero.png"
             alt="hero"
             width={1000}
             height={1000}
-            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+            className="max-h-[70vh] 2xl:max-h-[50vh] object-contain object-center hover:scale-110 transition-transform duration-500 ease-in-out shadow-lg rounded-lg hover:shadow-2xl"
           />
         </div>
-      </section> 
+      </section>
 
-      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Trust by <br /> Thousands of Events
+        </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
           <CategoryFilter />
         </div>
 
-        <Collection 
+        <Collection
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -62,5 +71,5 @@ export default async function Home({ searchParams }: SearchParamProps) {
         />
       </section>
     </>
-  )
+  );
 }
