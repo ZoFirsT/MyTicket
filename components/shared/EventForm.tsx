f8e553a976bc037d18f6d7d9fba0e8fc79392967
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-01-21 00:00:26 +0700

    EventForm

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -266,7 +266,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                         height={24}
                         className="filter-grey"
                       />
-                      <Input type="number" placeholder="Price" {...field} className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0" />
+                      <Input type="number" placeholder="Price <= 1 (มูลค่าอย่างน้อย 1 ดอลล่าห์)" {...field} className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0" />
                       <FormField
                         control={form.control}
                         name="isFree"
