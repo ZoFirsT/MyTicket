796c998bfcf97c385e9159c168802c1b0b5c2469
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-01-22 17:58:41 +0700

    updatespeed

--- app/(root)/test/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/TAnOFEzaw5-nuaI1/scene.splinecode" />
-  );
-}
