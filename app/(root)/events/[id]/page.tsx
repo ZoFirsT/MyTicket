f744b2df15b8e761cf8ad41558c9a8fcee3e0ddb
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-02-17 22:42:49 +0700

    update image event res

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -18,14 +18,13 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
   return (
     <>
     <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
-        <Image 
-          src={event.imageUrl}
-          alt="hero image"
-          width={1000}
-          height={1000}
-          className="h-full min-h-[300px] object-cover object-center"
-        />
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+          <Image
+            src={event.imageUrl}
+            alt="hero image"
+            layout="fill"
+            className="object-cover object-center"
+          />
 
         <div className="flex w-full flex-col gap-8 p-5 md:p-10">
           <div className="flex flex-col gap-6">
